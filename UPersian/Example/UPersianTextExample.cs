1c30756f16d211c3ba2463ba3c7640d9e8b2a65b
ElectroGryphon <electrogryphon@gmail.com>
2016-06-14 16:50:27 +0430

    UPersian v0.1.1
    Unity RTL Support

---
+++ UPersian/Example/UPersianTextExample.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UPersian.Components;
+using UPersian.Utils;
+
+public class UPersianTextExample : MonoBehaviour
+{
+
+    [SerializeField] private RtlText upText;
+
+	void Awake ()
+	{
+        // Set Rtl Text without any additional changes.
+	    upText.text = "سلام بدون نیاز به تغییر متن!";
+
+        // returns modified text. (Reverse and line fix to show correctly in Ui)
+        print(upText.text);
+
+        // BaseText returns original text. you sould use BaseText if you want to post rtl usernames to server or anywhere you need original rtl text value
+        print(upText.BaseText);
+
+        // keep in mind that you allways can fix original BaseText and use whereever you want using .RtfFix()
+        print(upText.BaseText.RtlFix());
+	}
+}
