0d5d5c238e9e1bc1aedc1caf98625fe1ceeea7c3
Omid <omid3098@gmail.com>
2018-02-09 01:29:41 +0330

    Fix Ya character in Persian

--- UPersian/Scripts/Utils/UPersianUtils.cs
+++ UPersian/Scripts/Utils/UPersianUtils.cs
@@ -15,14 +15,17 @@ namespace UPersian.Utils
         public static string RtlFix(this string str)
         {
             //ﺉﻚﻙﯤ
-            str = str.Replace('ی', 'ى');
+            str = str.Replace('ی', 'ي');
+            // str = str.Replace('ی', 'ی');
             //str = str.Replace( 'ی','ﺉ');
             
-            str = str.Replace('ک', 'ﻙ');
+            // str = str.Replace('ک', 'ﻙ');
             //str = str.Replace('ﻚ', 'ک');
-            str = ArabicSupport.ArabicFixer.Fix(str, true, false);
-
-            str = str.Replace('ﺃ', 'آ');
+            str = ArabicSupport.ArabicFixer.Fix(str);
+            str = str.Replace('ي', 'ى');
+            str = str.Replace('ﻲ', 'ﻰ');
+            
+            // str = str.Replace('ﺃ', 'آ');
             return str;
         }
 
