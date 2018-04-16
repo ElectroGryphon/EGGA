dd1251a6967bab5619a4aa828f195429701b577e
Omid <omid3098@gmail.com>
2018-04-16 17:34:09 +0430

    Added Active field to active/deactive rtl formatting

--- UPersian/Scripts/Components/RtlText.cs
+++ UPersian/Scripts/Components/RtlText.cs
@@ -5,22 +5,24 @@ using UnityEngine;
 using UnityEngine.UI;
 using UPersian.Utils;
 
-namespace UPersian.Components
-{
+namespace UPersian.Components {
     /// <summary>
     /// Supports RTL and middle-eastern languages text
     /// </summary>
-    [AddComponentMenu("UI/RtlText")]
-    public class RtlText : Text
-    {
+    [AddComponentMenu ("UI/RtlText")]
+    public class RtlText : Text {
         protected char LineEnding = '\n';
+
+        /// <summary>
+        /// Deactive to disable rtl formatting.
+        /// </summary>
+        public bool active = true;
 
         /// <summary>
         /// Original text which user sets via editor.
         /// You sould use this value if you want need original string. (to use in a third-party)
         /// </summary>
-        public string BaseText
-        {
+        public string BaseText {
             get { return base.text; }
         }
 
@@ -28,41 +30,35 @@ namespace UPersian.Components
         /// get: Return RTL fixed string
         /// set: Sets base.text
         /// </summary>
-        public override string text
-        {
-            get
-            {
+        public override string text {
+            get {
                 // Populate base text in rect transform and calculate number of lines.
                 string baseText = base.text;
-                cachedTextGenerator.Populate(baseText, GetGenerationSettings(rectTransform.rect.size));
+                if (!active) return baseText;
+                cachedTextGenerator.Populate (baseText, GetGenerationSettings (rectTransform.rect.size));
                 // Make list of lines
                 List<UILineInfo> lines = cachedTextGenerator.lines as List<UILineInfo>;
                 if (lines == null) return null;
-                if (lines.Count <= 1) return baseText.RtlFix();
+                if (lines.Count <= 1) return baseText.RtlFix ();
                 string linedText = "";
-                for (int i = 0; i < lines.Count; i++)
-                {
+                for (int i = 0; i < lines.Count; i++) {
                     // Find Start and Length of RTL line and append Line Ending character.
-                    if (i < lines.Count - 1)
-                    {
+                    if (i < lines.Count - 1) {
                         int startIndex = lines[i].startCharIdx;
                         int length = lines[i + 1].startCharIdx - lines[i].startCharIdx;
-                        linedText += baseText.Substring(startIndex, length);
+                        linedText += baseText.Substring (startIndex, length);
                         if (linedText.Length > 0 &&
                             linedText[linedText.Length - 1] != '\n' &&
-                            linedText[linedText.Length - 1] != '\r')
-                        {
+                            linedText[linedText.Length - 1] != '\r') {
                             linedText += LineEnding;
                         }
-                    }
-                    else
-                    {
+                    } else {
                         // For the Last line, we only need startIndex and line continues to the end.
-                        linedText += baseText.Substring(lines[i].startCharIdx);
+                        linedText += baseText.Substring (lines[i].startCharIdx);
                         //if (resizeTextForBestFit) linedText += '\n';
                     }
                 }
-                return linedText.RtlFix();
+                return linedText.RtlFix ();
             }
             set { base.text = value; }
         }
